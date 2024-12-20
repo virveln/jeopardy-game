932c5b6a0b61cf5ad96d4ed21902c3259ae2bbd5
Jessica Hvirfvel <jessica.hvirfvel@hotmail.com>
2024-12-20 20:48:46 +0100

    update font and title

--- src/components/StartPage.js
+++ src/components/StartPage.js
@@ -28,8 +28,28 @@ export default function StartPage({ goToGameSettings, goToInstructions, goToAttr
     return (
         <div className="start-page background-start">
             <StarTrail />
+            <div className="jeopardy-name">
+                <svg width="750" height="350" viewBox="0 0 750 350">
+                    <path id="arcPathTop" d="M 50,200 Q 375,70 700,200" fill="transparent" stroke="transparent" />
+
+                    <path id="arcPathBottom" d="M 50,330 Q 375,200 700,330" fill="transparent" stroke="transparent" />
+
+                    <text fill="#fff" font-size="110" font-family="Rammetto One" stroke="#A31ACB" stroke-width="3" text-anchor="middle">
+                        <textPath href="#arcPathTop" startOffset="50%">
+                            WHAT IS..
+                        </textPath>
+                    </text>
+
+                    <text fill="#fff" font-size="70" font-family="Rammetto One" stroke="#A31ACB" stroke-width="3" text-anchor="middle">
+                        <textPath href="#arcPathBottom" startOffset="50%">
+                            JEOPARDY?
+                        </textPath>
+                    </text>
+                </svg>
+            </div>
+
             {/* <h1 className='start-title'>JEOPARDY</h1> */}
-            <img className="jeopardy-name" src={jeopardyName} alt="jeopardy" />
+            {/* <img className="jeopardy-name" src={jeopardyName} alt="jeopardy" /> */}
             <div className="btn-start-container">
                 <button className="btn btn-startpage" onClick={goToGameSettings}>Play Game</button>
                 <button className="btn btn-startpage" onClick={goToInstructions}>Instructions</button>
